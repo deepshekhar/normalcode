f19216325e76524da00d0bdcb0f83629ff8fb9f4
deepshekhar <55529788+deepshekhar@users.noreply.github.com>
2020-04-18 19:20:58 +0530

    Add files via upload

---
+++ xp2.py
@@ -0,0 +1,13 @@
+name=input("enter a file name")                 #program to read a file and arrange the words in a list in alphabetical order.There
+fname=open(name)                                        #is no repetation of words and each word is taken once
+di=dict()
+
+for line in fname:
+    words=line.split()
+    for word in words:
+        di[word]=di.get(word,0) + 1
+lst=list()
+for key,value in di.items():
+    lst.append(key)
+lst.sort()
+print(lst)
